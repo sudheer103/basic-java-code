300bd39fb8b42832597a48e4fa6a894db6ef93bb
sudheer103 <sudheerjames13@gmail.com>
2024-12-12 14:38:36 +0530

    adding loop to my project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("sudheer");
+        System.out.println("Aslam");
+        for (int i = 0; i < 10; i++) {
+            if (i%2==0) {
+                System.out.println("Lokesh");
+            }else{
+                System.out.println("sudheer");
+            }
+        }
+        System.out.println("Satish");
     }
 }
